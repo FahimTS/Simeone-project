ba506d56ba126c7c2f926216cc168c72411f7efd
Md. Fahim Hossain Sujon <120704636+FahimTS@users.noreply.github.com>
2023-05-14 14:20:32 +0800

    Just practice project
    Just practice project not responsive

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    strings: ["I'm Fahim Hossen.", "I'm a Web Developer.", "I'm a Digital Marketer."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
